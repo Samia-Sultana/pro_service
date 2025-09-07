8c0c49add09591dfb792c8a7ff038ce806315122
Samia-Sultana <samia.cse.ewu@gmail.com>
2025-09-07 13:57:19 +0600

    package crud done

--- app/Http/Controllers/Admin/CategoryPackageController.php
+++ app/Http/Controllers/Admin/CategoryPackageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Interfaces\Admin\CategoryPackageServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Validator;
 
 class CategoryPackageController extends Controller
 {
@@ -28,6 +30,87 @@ class CategoryPackageController extends Controller
             'data' => $data
         ]);
     }
+
+    public function allPackage(){
+
+        $data = $this->packageService->allPackage();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Data retrieved successfully',
+            'data' => $data
+        ]);
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|unique:category_packages,name',
+            'tag' => 'required|string|max:255|unique:category_packages,tag',
+            'category_id' => 'nullable|exists:categories,id',
+            'description' => 'required|string|max:255',
+            'image' => [
+                'image',
+                'mimes:jpeg,png,jpg',
+                'max:2048',
+            ],
+            'price' => 'required',
+            'discount' => 'nullable',
+        ]);
+
+        if ($validator->fails()) {
+    return response()->json([
+        'message' => 'Validation failed',
+        'errors' => $validator->errors()
+    ], 422);
+        }
+
+        $packageData = $validator->validated();
+        $package = $this->packageService->store($packageData);
+        return response()->json([
+            'status' => 200,
+            'message' => 'package created successfully',
+            'data' => $package
+        ]);
+
+    }
+
+     public function edit(Request $request){
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|exists:category_packages,id',
+            'name' => [
+                'required', 'string', Rule::unique('category_packages', 'name')->ignore($request->id),
+            ],
+            'tag' => [
+                'required', 'string', Rule::unique('category_packages', 'tag')->ignore($request->id),
+            ],
+            'category_id' => 'nullable|exists:categories,id',
+
+
+            'description' =>'required|string|max:255',
+
+            'image' => [
+                'nullable', 'image', 'mimes:jpeg,png,jpg', 'max:2048',
+            ],
+            'price' => 'required',
+            'discount' => 'nullable',
+        ]);
+
+         if ($validator->fails()) {
+    return response()->json([
+        'message' => 'Validation failed',
+        'errors' => $validator->errors()
+    ], 422);
+        }
+
+        $packageData = $validator->validated();
+        $package = $this->packageService->edit($packageData);
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'package updated successfully',
+            'data' => $package
+        ]);
+    }
+
 
     public function packageDetail($id){
         $data = $this->packageService->packageDetail($id);
--- routes/admin/package.php
+++ routes/admin/package.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Admin\CategoryPackageController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth:user'], function(){
-    Route::get('/packages', action: [CategoryPackageController::class, 'index'])->name('packages');
+    Route::get('/packages', [CategoryPackageController::class, 'index'])->name('packages');
+    Route::get('/package/all',  [CategoryPackageController::class, 'allPackage'])->name('package.all');
     Route::post('/package/store', [CategoryPackageController::class, 'store'])->name('package.store');
     Route::get('/package/{id}', [CategoryPackageController::class, 'packageDetail'])->name('package.detail');
     Route::post('/package/edit', [CategoryPackageController::class, 'edit'])->name('package.edit');
